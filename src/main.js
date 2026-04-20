fa76b6836172df1e1af5821ce3d0177456e82112
rinziv <rinzivillo@isti.cnr.it>
2026-04-20 14:34:32 +0200

    introducing de.scaleLinear for x axis

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import * as d3 from "d3";
 
+const MAX_VAL = 400;
+
 let numbers = [13, 34, 24, 5, 8, 15, 20];
+
 
 const svg = d3.select("#app")
   .append("svg")
@@ -11,15 +14,19 @@ function posy(d, i) {
     return i * 10;
   }  
 
-function posx(d) {
-  return d * 10;
-}
-
+const xScale = d3.scaleLinear()
+  .domain([0, MAX_VAL])
+  .range([0, 483]);
 
 d3.select("#update").on("click", function() {
   const num = Math.floor(Math.random() * 10);
-  numbers = d3.range(num).map(n => Math.floor(Math.random() * 40) );
+  numbers = d3.range(num).map(n => Math.floor(Math.random() * MAX_VAL) );
   console.log('numbers', numbers);
+
+  // OPTION 1: maintain xScale fixed
+  // OPTION 2: 
+  // const new_max = d3.max(numbers);
+  // xScale.domain([0, new_max]);
 
   // let lines = svg.selectAll("line")
   // .data(numbers);
@@ -39,7 +46,7 @@ d3.select("#update").on("click", function() {
     .attr("x1", 0)
     .attr("y1", posy)
     .attr("y2", posy)
-    .attr("x2", posx)
+    .attr("x2", xScale)
     .attr("stroke", "black")
     .attr("stroke-width", 2);
 });
