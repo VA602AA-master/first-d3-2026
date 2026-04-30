bb681d254a66eee62c9f0d9fd648ac07363ec466
rinziv <rinzivillo@isti.cnr.it>
2026-04-30 17:39:51 +0200

    refactor: restructure data fetching and bar chart rendering logic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import * as d3 from "d3";
 
 const MAX_VAL = 400;
+const API_URL = "http://localhost:8000";
+const DEFAULT_GRAPH_NAME= "default-graph";
 
 let numbers = [13, 34, 24, 5, 8, 15, 20];
 
@@ -10,66 +12,79 @@ const svg = d3.select("#app")
   .attr("width", 500)
   .attr("height", 500);
 
+const barchart = BarChart();
+
+fetch(`${API_URL}/set-default/${DEFAULT_GRAPH_NAME}`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: {
+    "key": "value",
+  }
+}).then(res => res.json())
+  .then(data => {
+    console.log('data', data);
+    d3.select("#update").on("click", function() {
+      fetch(`${API_URL}/node-types/default`)
+        .then(res => res.json())
+        .then(data => {
+          console.log('data', data);
+          svg.datum(
+            Object.values(
+              data.node_type_counts
+            )
+          )
+          .call(barchart);
+        })
+    });    
+  })
+  .catch(err => console.error('Error:', err));  
 
 
-const xScale = d3.scaleLinear()
-  .domain([0, MAX_VAL])
-  .range([0, 480]);
-
-const yScale = d3.scaleBand()
-  // .domain()
-  .range([0, 480])
-  .paddingInner(0.3)
-  .paddingOuter(0.3);
-
-// function posy(d, i) {
-//     return yScale(i);
-//   } 
 
 
-d3.select("#update").on("click", function() {
-  const num = Math.floor(Math.random() * 80);
-  numbers = d3.range(num).map(n => Math.floor(Math.random() * MAX_VAL) );
-  console.log('numbers', numbers);
 
-  yScale.domain(d3.range(num));
+function BarChart(){
 
-  // OPTION 1: maintain xScale fixed
-  // OPTION 2: 
-  // const new_max = d3.max(numbers);
-  // xScale.domain([0, new_max]);
+  const xScale = d3.scaleLinear()
+    .domain([0, MAX_VAL])
+    .range([0, 480]);
 
-  // let lines = svg.selectAll("line")
-  // .data(numbers);
+  const yScale = d3.scaleBand()
+    // .domain()
+    .range([0, 480])
+    .paddingInner(0.3)
+    .paddingOuter(0.3);
 
-  // lines.exit().remove(); 
+  function my(selection){
+    yScale.domain(d3.range(selection.datum().length));
+    xScale.domain([0, d3.max(selection.datum())]);
 
-  // lines = lines
-  //   .enter()
-  //   .append("line")
-  //   .merge(lines);
+    let gs = selection.selectAll("g.bars")
+      .data(selection.datum())
+      .join("g")
+      .classed('bars', true)
+      .attr('transform', (d, i) => `translate(0, ${yScale(i)})`)
+  
 
-  let gs = svg.selectAll("g.bars")
-    .data(numbers)
-    .join("g")
-    .classed('bars', true)
-    .attr('transform', (d, i) => `translate(0, ${yScale(i)})`)
- 
+    gs.selectAll('rect')
+      .data(d => [d])
+      .join('rect')
+      // .attr("x", 0)
+      // .attr("y", (d, i) => yScale(i))
+      .attr("height", yScale.bandwidth())
+      .attr("width", xScale)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2);
+  }
 
-  gs.selectAll('rect')
-    .data(d => [d])
-    .join('rect')
-    // .attr("x", 0)
-    // .attr("y", (d, i) => yScale(i))
-    .attr("height", yScale.bandwidth())
-    .attr("width", xScale)
-    .attr("stroke", "black")
-    .attr("stroke-width", 2);
+  return my;
+}
+
+// introducing modules
 
 
-});
-
-// step 4: adjust layout (introducing gs)
 
 
 
